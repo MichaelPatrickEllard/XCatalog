245f14ad1ace8e626d879266c10e8fd4541046a0
Michael Patrick Ellard <ellard@rescuemissionsoftware.com>
2015-07-11 08:54:39 -0700

    Adding OS X target to project

---
+++ Swift/XCatalogOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  XCatalogOSX
+//
+//  Created by Rescue Mission Software on 7/11/15.
+//  Copyright (c) 2015 Apple. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
