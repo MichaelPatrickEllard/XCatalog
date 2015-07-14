478a10b2b4af65bc0565b95172aaeb680f60b64d
Mike Mehr <mmehr2x-dev@yahoo.com>
2015-07-13 17:37:52 -0700

    Trying to implement NSTableView version of this, but big problems came up. Seems like useStaticContents isn't working in Yosemite (is there a radar?) and I can't get programmatic loading to work on two tables in the XIB. Putting it on a branch and doing something else instead.

--- Swift/XCatalogOSX/AlertControllerViewController.swift
+++ Swift/XCatalogOSX/AlertControllerViewController.swift
@@ -10,9 +10,92 @@ import Cocoa
 
 class AlertControllerViewController: NSViewController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var tableView2: NSTableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do view setup here.
+        /*
+        // Following feature was introduced in Yosemite and doesn't work yet (file radar?)
+        tableView.usesStaticContents = true
+        tableView2.usesStaticContents = true
+        */
+        tableView.setDataSource(self)
+        tableView2.setDataSource(self)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Do view setup here.
+        /*
+        // Following feature was introduced in Yosemite and doesn't work yet (file radar?)
+        tableView.usesStaticContents = true
+        tableView2.usesStaticContents = true
+        */
+        tableView.setDataSource(self)
+        tableView2.setDataSource(self)
+    }
+
+    
+    /// Show an alert with an "Okay" button.
+//    func showSimpleAlert(_: NSIndexPath) {
+//        let title = NSLocalizedString("A Short Title is Best", comment: "")
+//        let message = NSLocalizedString("A message should be a short, complete sentence.", comment: "")
+//        let cancelButtonTitle = NSLocalizedString("OK", comment: "")
+//        
+//        let alertController = UIAlertController(title: title, message: message, preferredStyle: .Alert)
+//        
+//        // Create the action.
+//        let cancelAction = UIAlertAction(title: cancelButtonTitle, style: .Cancel) { action in
+//            NSLog("The simple alert's cancel action occured.")
+//        }
+//        
+//        // Add the action.
+//        alertController.addAction(cancelAction)
+//        
+//        presentViewController(alertController, animated: true, completion: nil)
+//    }
+    
+}
+
+extension AlertControllerViewController: NSTableViewDataSource {
+    // MARK: NSTableViewDataSource protocol compliance
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        if tableView === self.tableView {
+            return 7
+        }
+        if tableView === self.tableView2 {
+            return 2
+        }
+        return 0
+    }
+
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        if let columnIdentifier = tableColumn?.identifier {
+            var rowNum = -1
+            if tableView === self.tableView {
+                rowNum = row
+            }
+            if tableView === self.tableView2 {
+                rowNum = row + numberOfRowsInTableView(self.tableView) // 5
+            }
+            if rowNum >= 0 {
+                // one of our tableViews and the row number is specified
+                let identifier = "row\(rowNum+1)"
+                if let cellView = tableView.makeViewWithIdentifier(identifier, owner: self) as? NSTableCellView {
+                    cellView.textField?.stringValue = "TEST\(rowNum+1)"
+                    cellView.identifier = identifier
+                    return cellView
+                }
+            }
+        }
+        return nil
     }
     
 }
+
+extension AlertControllerViewController: NSTableViewDelegate {
+    // MARK: NSTableViewDelegate protocol compliance
+}
+
